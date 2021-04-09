8a500339825616dd2691c0723e236f0f4fda968b
Jørund <noen54@gmail.com>
2021-04-09 20:50:35 +0200

    api.py small changes

--- api.py
+++ api.py
@@ -12,35 +12,39 @@ users = [
     {'id': "5a93949a308b4570", 'username': 'Dhalgren'}
 ]
 
-@app.route('/api/users', methods=['GET', 'SET'])
+
+@app.route('/api/users', methods=['GET', 'POST'])
 def usr():
     # Returns all registered users in json
     if request.method == 'GET':
         return jsonify(users)
     
     # Registers an user
-    if request.method == 'SET':
-        newUser = { "id": uuid.uuid4().hex[:16], "username": request.json["username"] }
-        users.append(newUser)
+    if request.method == 'POST':
+        new_user = {"id": uuid.uuid4().hex[:16], "username": request.json["username"]}
+        users.append(new_user)
         return jsonify(users)
+
 
 @app.route('/api/user/<string:user_id>', methods=['GET', 'DELETE'])
 def usr_id(user_id):
     # Checks if selected id exists, if not throws an exception
-    selectedUser = None
+    selected_user = None
     for user in users:
         if user["id"] == user_id:
-            selectedUser = user
-    if selectedUser == None:
+            selected_user = user
+
+    if selected_user is None:
         abort(404)
 
     # Return user with specific id
     if request.method == 'GET':
-        return selectedUser
+        return selected_user
 
     # Delete user with specific id
     if request.method == 'DELETE':
-        users.remove(user)
+        users.remove(selected_user)
         return jsonify(users)
+
 
 app.run()
