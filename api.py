dbcee20c13f6f9f95dae5a52af5687cd6531b9c6
Nikola Dordevic <Dordenik1@gmail.com>
2021-04-09 17:04:23 +0200

    Work on api users, not complete

--- api.py
+++ api.py
@@ -15,10 +15,24 @@ users = [
 
 @app.route('/api/users', methods=['GET', 'SET'])
 def usr():
+    # Returns all registered users in json
     if request.method == 'GET':
         return jsonify(users)
     
+    # Registers an user
     if request.method == 'SET':
-        return "GOT USER"
+        newUser = { "id": len(users), "username": request.args.get('username') }
+        users.append(newUser)
 
+        return jsonify(users)
+
+"""
+@app.route('/api/user/<user-id>', methods=['GET', 'SET'])
+def usr_id():
+    # Return user with specific id
+    if request.method == 'GET':
+
+    # Delete user with specific id
+    return
+"""
 app.run()
