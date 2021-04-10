dbcad8eb12a8ac8e1f79fbf1e7fb65d2bad512ce
Jørund <noen54@gmail.com>
2021-04-10 12:55:08 +0200

    added start room

--- api.py
+++ api.py
@@ -12,6 +12,10 @@ users = [
     {'id': "5a93949a308b4570", 'username': 'Dhalgren'}
 ]
 
+rooms = [
+    {'id': "d45581f3a63f4b0a", "name": "Test", "creator_id": "d45581f3a63f4b0b"}
+]
+
 
 @app.route('/api/users', methods=['GET', 'POST'])
 def usr():
@@ -33,6 +37,7 @@ def usr_id(user_id):
     for user in users:
         if user["id"] == user_id:
             selected_user = user
+            break
 
     if selected_user is None:
         abort(404)
@@ -45,6 +50,45 @@ def usr_id(user_id):
     if request.method == 'DELETE':
         users.remove(selected_user)
         return jsonify(users)
+
+
+@app.route('/api/rooms', methods=['GET', 'POST'])
+def roo():
+    # Returns all rooms in json
+    if request.method == 'GET':
+        return jsonify(rooms)
+
+    # Creates a new room
+    if request.method == 'POST':
+        new_room = {"id": uuid.uuid4().hex[:16], "name": "", "creator_id": ""}
+        users.append(new_room)
+        return jsonify(rooms)
+
+
+@app.route("/api/room/<string:room_id>", methods=["GET", "DELETE"])
+def roo_id(room_id):
+    selected_room = None
+    user_id = ""
+    for room in rooms:
+        if room["id"] == room_id:
+            selected_room = room
+            break
+
+    # Room doesn't exist
+    if selected_room is None:
+        abort(404)
+
+    # Returns room
+    if request.method == "GET":
+        return selected_room
+
+    # Deletes room if user_id was the same as the room creator
+    if request.method == "DELETE":
+        if user_id == selected_room["creator_id"]:
+            rooms.remove(selected_room)
+            return jsonify(rooms)
+        else:
+            abort(400)
 
 
 app.run()
