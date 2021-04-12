54787db14409a19b5ad7264d75cb8c2bfa4b1a12
Jørund <noen54@gmail.com>
2021-04-12 20:30:42 +0200

    understrek fuksjonsnavn pluss innlogging sjekk

--- client.py
+++ client.py
@@ -4,8 +4,8 @@ import sys
 import time
 
 api_url = sys.argv[1]
-user_id = "d45581f3a63f4b0b"
-room_id = "d45581f3a63f4b0a"
+user_id = None
+room_id = None
 running = True
 old_message_array = []
 first_time_thread = 0
@@ -25,16 +25,19 @@ def commands(msg):
     # Get a list of all commands
     if command[0] == "/help":
         return ("""
+            /help - provides info of all commands
             /register <username> - Registers a user
             /login <user_id> - Login as a user
             /join <room_id> - Join a specific room with room_id
             /create <room_name> - Create a room with the given name
-            /getRooms - Get all rooms
-            /getRoomUsers <room_id> - Get all users in a specific room (That you are in)
+            /delete_user <user_id> - Deletes user with given user_id, has to be user
+            /delete_room <room_id> - Deletes room with giver room_id, has to be room creator
+            /get_rooms - Get all rooms
+            /get_room_users <room_id> - Get all users in a specific room (That you are in)
             """)
 
     # Register as an user
-    elif command[0] == "/register":  # <username>
+    elif command[0] == "/register":
         request = requests.post("{}/api/users".format(api_url), json={"username": command[1]})
         if request.status_code == 200:
             user_id = request.json()["id"]
@@ -58,70 +61,59 @@ def commands(msg):
             user_id = None
             return "An unexpected error occurred"
 
-    # Join a room as a registered user
-    elif command[0] == "/join":  # <room_id>
-        req = None
-        try:
-            req = requests.get("{}/api/room/{}".format(api_url, command[1]), json={"user_id": user_id})
-            room_id = req.json()["id"]
-            old_message_array = []
-            if first_time_thread == 0:
-                first_time_thread = 1
-                receive_thread = threading.Thread(target=receive_message)
-                receive_thread.start()
-            print("Changed room to {}".format(get_room(command[1])["name"]))
+    elif user_id is None:
 
-        except:
-            print("\nSyntax or room_id is not valid. Type /help for more info")
-            if req.status_code == 404:
-                print("Room does not exist")
-            elif req.status_code == 400:
-                print("No provided user id")
+        # Join a room as a registered user
+        if command[0] == "/join":  # <room_id>
+            req = None
+            try:
+                req = requests.get("{}/api/room/{}".format(api_url, command[1]), json={"user_id": user_id})
+                room_id = req.json()["id"]
+                old_message_array = []
+                if first_time_thread == 0:
+                    first_time_thread = 1
+                    receive_thread = threading.Thread(target=receive_message)
+                    receive_thread.start()
+                print("Changed room to {}".format(get_room(command[1])["name"]))
 
+            except:
+                print("\nSyntax or room_id is not valid. Type /help for more info")
+                if req.status_code == 404:
+                    print("Room does not exist")
+                elif req.status_code == 400:
+                    print("No provided user id")
 
-    # Create a room
-    elif command[0] == "/create":  # <room_name>
-        req = None
-        try:
+        # Create a room
+        elif command[0] == "/create":  # <room_name>
             req = requests.post("{}/api/rooms".format(api_url), json={"user_id": user_id, "name": command[1]})
             print("Created a room with name: {} and id: {}".format(command[1], req.json()["id"]))
-        except:
-            if req.status_code == 400:
-                print("No provided user id")
 
-    # Delete user
-    elif command[0] == "/delete_user":
-        request = requests.delete("{}/api/user/{}".format(api_url, command[1]), json={"user_id": user_id})
-        if request.status_code == 404:
-            return "Invalid user id"
-        elif request.status_code == 400:
-            return "No user id provided"
-        elif request.status_code == 401:
-            return "You are not permitted to delete this user"
-        elif request.status_code == 200:
-            return "Deletion successful"
+        # Delete user
+        elif command[0] == "/delete_user":
+            request = requests.delete("{}/api/user/{}".format(api_url, command[1]), json={"user_id": user_id})
+            if request.status_code == 401:
+                return "You are not permitted to delete this user"
+            elif request.status_code == 200:
+                return "Deletion successful and you have been logged out"
 
-    # Delete room
-    elif command[0] == "/delete_room":
-        request = requests.delete("{}/api/room/{}".format(api_url, command[1]), json={"user_id": user_id})
-        if request.status_code == 404:
-            return "Invalid room id"
-        elif request.status_code == 400:
-            return "No user id provided"
-        elif request.status_code == 401:
-            return "You are not permitted to delete this room"
-        elif request.status_code == 200:
-            return "Deletion successful"
+        # Delete room
+        elif command[0] == "/delete_room":
+            request = requests.delete("{}/api/room/{}".format(api_url, command[1]), json={"user_id": user_id})
+            if request.status_code == 401:
+                return "You are not permitted to delete this room"
+            elif request.status_code == 200:
+                user_id = None
+                return "Deletion successful"
 
-    elif command[0] == "/getRooms":
-        for room in get_all_rooms().json():
-            print("id: {}   name: {}".format(room["id"], room["name"]))
+        # Gets rooms
+        elif command[0] == "/get_rooms":
+            for room in get_all_rooms().json():
+                print("id: {}   name: {}".format(room["id"], room["name"]))
 
-    elif command[0] == "/getRoomUsers": # <room_id>
-        for user in get_all_room_users(command[1]):
-            print("id: {}".format(user["id"]))
-
-
+        # Gets all users in a room
+        elif command[0] == "/get_room_users":  # <room_id>
+            for user in get_all_room_users(command[1]):
+                print("id: {}".format(user["id"]))
 
     else:
         print("Command does not exist")
@@ -147,11 +139,11 @@ def send_message():
 
 
 def get_room(wanted_room_id):
-    return requests.get("{}/api/room/{}".format(api_url, wanted_room_id), json={"user_id": user_id})
+    return requests.get("{}/api/room/{}".format(api_url, wanted_room_id), json={"user_id": user_id}).json()
 
 
 def get_user(wanted_user_id):
-    return requests.get("{}/api/user/{}".format(api_url, wanted_user_id), json={"user_id": user_id})
+    return requests.get("{}/api/user/{}".format(api_url, wanted_user_id), json={"user_id": user_id}).json()
 
 
 def get_all_users():
@@ -163,7 +155,12 @@ def get_all_rooms():
 
 
 def get_all_room_users(wanted_room_id):
-    return requests.get("{}/api/room/{}/users".format(api_url, wanted_room_id), json={"user_id": user_id}).json()["users"]
+    return requests.get("{}/api/room/{}/users".format(api_url, wanted_room_id), json={"user_id": user_id})
+
+
+# def get_all_room_messages(wanted_room_id):
+#    return requests.get("{}/api/room/{}/messages".format(api_url, wanted_room_id), json={"user_id": user_id})
+
 
 def receive_message():
     global running
