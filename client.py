35592c9210a7b2620372dc1fd97a7147cfa5158f
Nikola <Dordenik1@gmail.com>
2021-04-12 12:14:52 +0200

    still not working lmao

--- client.py
+++ client.py
@@ -5,8 +5,8 @@ import sys
 import time
 
 api_url = sys.argv[1]
-user_id = None
-room_id = None
+user_id = "d45581f3a63f4b0b"
+room_id = "d45581f3a63f4b0a"
 
 """
 def commands(msg):
@@ -26,12 +26,16 @@ def commands(msg):
         # Join a room as a registered user
         elif command[0] =="/join": # <room_id>
             return
+
+        # Create a room
+        elif command[0] =="/create": # <room_name>
+            return
         
         else:
             print("Command does not exist")
 """
 def username_of_id(wanted_user_id):
-    return requests.get(api_url +"/api/user/"+ wanted_user_id, data={"user_id: "+ user_id})["username"]
+    return requests.get("{}/api/user/{}".format(api_url, wanted_user_id), json={"user_id ": user_id})["username"]
 
 def send_message():
     try:
@@ -44,11 +48,12 @@ def send_message():
         print("send_message ended")
 
 def recieve_message():
-    new_message_array = requests.get(api_url + "/api/room/"+ room_id +"/messages", data={"user_id: "+ user_id})
+    new_message_array = requests.get("{}/api/room/{}/messages".format(api_url, room_id), json={"user_id": user_id})
     old_message_array = []
     
     try:
         while True:
+            # Finner bare de nye meldingene
             new_messages = [msg for msg in new_message_array if msg not in old_message_array]
             old_message_array = new_message_array
             time.sleep(0.5)
@@ -58,3 +63,6 @@ def recieve_message():
 
     except:
         print("recieve_message ended")
+
+send_thread = threading.Thread(target= recieve_message)
+send_thread.start()
