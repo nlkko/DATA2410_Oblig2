ab8435cb513eb6ff2425413ee3eb090ceaacbe77
Nikola <Dordenik1@gmail.com>
2021-04-12 13:53:10 +0200

    Send message done dunno not tested

--- client.py
+++ client.py
@@ -8,32 +8,30 @@ api_url = sys.argv[1]
 user_id = "d45581f3a63f4b0b"
 room_id = "d45581f3a63f4b0a"
 
-"""
 def commands(msg):
     command = msg.split(' ')
-        # Get a list of all commands
-        if command[0] == "/help":
-            return
+    # Get a list of all commands
+    if command[0] == "/help":
+        return
 
-        # Register as an user
-        elif command[0] == "/register": # <username>
-            return
+    # Register as an user
+    elif command[0] == "/register": # <username>
+        return
 
-        # Login as a user
-        elif command[0] == "/login": # <user_id>
-            return
+    # Login as a user
+    elif command[0] == "/login": # <user_id>
+        return
         
-        # Join a room as a registered user
-        elif command[0] =="/join": # <room_id>
-            return
+    # Join a room as a registered user
+    elif command[0] =="/join": # <room_id>
+        return
 
-        # Create a room
-        elif command[0] =="/create": # <room_name>
-            return
+    # Create a room
+    elif command[0] =="/create": # <room_name>
+        return
         
-        else:
-            print("Command does not exist")
-"""
+    else:
+        print("Command does not exist")
 
 def send_message():
     try:
@@ -42,6 +40,8 @@ def send_message():
 
             if msg[0] == "/":
                 commands(msg)
+            else:
+                requests.post("{}/api/room/{}/{}/messages".format(api_url, room_id, user_id), json={"user_id": user_id, "message": msg})
     except:
         print("send_message ended")
 
@@ -49,18 +49,25 @@ def get_user(wanted_user_id):
     return requests.get("{}/api/user/{}".format(api_url, wanted_user_id), json={"user_id": user_id}).json()
 
 def receive_message():
-    new_message_array = requests.get("{}/api/room/{}/messages".format(api_url, room_id), json={"user_id": user_id}).json()
-    old_message_array = []
+    try:
+        new_message_array = []
+        old_message_array = []
 
-    while True:
-        # Finner bare de nye meldingene
-        new_messages = [msg for msg in new_message_array if msg not in old_message_array]
-        old_message_array = new_message_array
-        time.sleep(0.5)
+        while True:
+            # Finner bare de nye meldingene
+            new_message_array = requests.get("{}/api/room/{}/messages".format(api_url, room_id), json={"user_id": user_id}).json()
+            new_messages = new_message_array[(len(old_message_array)):]
+            old_message_array = new_message_array
+            time.sleep(0.5)
 
-        if new_message_array != []:
-            for msg in new_messages:
-                print(get_user(msg["user_id"])["username"] +": " + msg["message"])
+            if new_message_array:
+                for msg in new_messages:
+                    print(get_user(msg["user_id"])["username"] +": " + msg["message"])
+    except:
+        print("send_thread stopped")
 
-send_thread = threading.Thread(target= receive_message)
+receive_thread = threading.Thread(target=receive_message)
+receive_thread.start()
+
+send_thread = threading.Thread(target=send_message)
 send_thread.start()
